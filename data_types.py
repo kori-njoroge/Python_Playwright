0d324394d6c871d63505b3ab734783d6cc9ecc3b
Kori <korinjoroge63@gmail.com>
2025-03-27 13:16:24 +0300

    Python data types

---
+++ data_types.py
@@ -0,0 +1,8 @@
+# strings -> "Hello", 'Hello, '1234', "23dfr)*&^&"
+print("Hello")
+
+# Integer  - 1 , 200, 1234
+print(12+2)
+
+# Float -> 2.4, 0.1123
+print(1.4+1)
--- hello.py
+++
@@ -1,7 +0,0 @@
-msg = "Roll a Dice"
-
-print(msg)
-def roll_dice():
-
-def roll_aanother():
-    
