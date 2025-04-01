89e06b5f5a0703ff092cbf61f5d266182a0b5bc1
Kori <korinjoroge63@gmail.com>
2025-04-01 09:51:39 +0300

    Using varibles

--- Files/variables.py
+++ Files/variables.py
@@ -7,3 +7,10 @@ print(temperature)
 print(name)
 print(age)
 print(temperature)
+
+# Use snake case when working with vairables
+# Concatenation
+greetings = "Hello"
+name = "Zoey"
+
+print(greetings + " " + name)
