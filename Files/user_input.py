1e5b3bd5d7d2b17a678285f0cd22911a25604411
Kori <korinjoroge63@gmail.com>
2025-04-09 13:18:49 +0300

    user input, conversion to string and int and vice versa

--- Files/user_input.py
+++ Files/user_input.py
@@ -1,3 +1,17 @@
-user_name = input("Enter your name here:")
-
+# user_name = input("Enter your name here:")
+user_name = 'Gideon'
 print(f' Your username is {user_name}')
+
+# Data type conversion
+print( 1+1)
+
+print('1' + '1')
+
+# int() - function used to convert string to Interger
+# str() function used to convert interger to string data type
+
+print(int('1')+int('1'))
+
+user_input = input("Type a number: ")
+
+print(int(user_input) + 5)
