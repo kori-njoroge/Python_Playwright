8bba804a5499c7512624629821ea840b95a8f7ed
Kori <korinjoroge63@gmail.com>
2025-04-09 13:57:53 +0300

    boolean logic on combined operators

--- Files/booleans_and_comparisons.py
+++ Files/booleans_and_comparisons.py
@@ -12,3 +12,27 @@ print('s' == 'S')
 print('s' > 'S')
 
 print( "Gideon" ==  "gideon")
+
+# boolean Logic
+print ("************Boolean Logic***********")
+a = 1
+b = 2
+c = 3
+
+# and - all the values have to be true for the statement to return a true.
+print(a == 11 and b == 21 and c == 33)
+print(a == 1 and b == 2 and c == 3)
+
+# or - either of the statements have to be true to return a true
+print(a == 1 or b == 21 or c == 3)
+print(a == 1 or b == 21 or c == 37)
+
+
+print("***Combining them all together+***")
+#Combining 'or' and 'and'
+print(a == 1 or b == 21 and c == 37)
+print(a == 1 or b == 21 and c == 3)
+print(a == 1 or ( b == 21 and c == 3))
+print((a == 1 or b == 21 )and c == 31)
+
+
