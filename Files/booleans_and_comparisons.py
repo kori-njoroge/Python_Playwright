7df2a52342ed3973a6cb7cb88852029806c716cc
Kori <korinjoroge63@gmail.com>
2025-04-09 13:32:47 +0300

    booleans and comparisons

---
+++ Files/booleans_and_comparisons.py
@@ -0,0 +1,14 @@
+# Booleans
+# integers
+print(2 == 2)
+print(2 != 2)
+print(2 > 3)
+print(2 <= 3)
+print(2.9 <= 2.3)
+
+# strings - this is case sentitive (pythin is UTF8 encoded)
+print('s' == 's')
+print('s' == 'S')
+print('s' > 'S')
+
+print( "Gideon" ==  "gideon")
