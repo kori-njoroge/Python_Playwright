53f11ea750a1a55fda6f9be57d2cb9dd5e4e52a5
Kori <korinjoroge63@gmail.com>
2025-04-09 13:46:04 +0300

    elif

--- Files/conditional_statements.py
+++ Files/conditional_statements.py
@@ -1,6 +1,18 @@
 a = 3
 
+# if else
 if a < 10:
     print('code was run')
 else:
     print("The statement is not true")
+    
+# else if
+
+if a < 10:
+    print('3 is less than 10')
+elif a > 10:
+    print("3 is greater than 10")
+elif a == 10:
+    print("3 equals to 10")
+else:
+    print("Invalid scenerio")
