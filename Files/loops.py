ec7c91a9022a581da71964a06fb6c94483b29399
Kori <korinjoroge63@gmail.com>
2025-04-30 13:31:27 +0300

    for Loop'

---
+++ Files/loops.py
@@ -0,0 +1,18 @@
+# FOR LOOPS
+for item in "Hello!":
+    print(item)
+
+
+for item in range(5):
+    print(item)
+    
+    
+names = ["Alice", "Ann", "Zoey"]
+
+for name in names:
+    print(f"Hello, {name}!")
+    if name == "Zoey":
+        print(f'{name} is my girlfriend')
+    else:
+        print("Hello there")
+        
