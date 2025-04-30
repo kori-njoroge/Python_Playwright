8c7306bf01262df71a7568e1df7062b2ceac265f
Kori <korinjoroge63@gmail.com>
2025-04-30 13:14:30 +0300

    disctionaries

--- Files/lists.py
+++ Files/lists.py
@@ -13,3 +13,17 @@ print(l.pop())
 # they are immutable, once created you stuck with it
 tuple_a = (23,24,25,1,2,54)
 
+# Distionaries
+# -> Structures in key: value format.
+our_dictionary = {"name":"Gideon", "age": 12, "List":[7,5,2], "inside_dictionary": {"greetings": "Hello"}}
+
+print (our_dictionary.values())
+
+# methods you can perform on a disctionary
+# .values()
+# .pop()
+# .push()
+# .get("name")
+# .update("address":"Kwetu")
+# .popitem()
+# .keys()
