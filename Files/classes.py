01996b802f56ba07bfc1b98a008c74ed2cda2a0c
Kori-njoroge <korinjoroge63@gmail.com>
2025-12-02 15:11:25 +0300

    inheritancee

--- Files/classes.py
+++ Files/classes.py
@@ -7,3 +7,22 @@ print (car1.brand)
 
 car2 = Car()
 print (car2.color)
+
+
+class Vehicle:
+    def __init__(self, brand, doors, wheels = 4 ):
+        self.brand = brand
+        self.doors = doors
+        self.wheels = wheels
+        
+    def car_greeting(self, local_slang):
+        print(f"Your car brand is {self.brand} aka {local_slang}")
+        
+
+Veh1 = Vehicle("VW", 2)
+
+veh2 = Vehicle("BMW", 2)
+
+
+
+
