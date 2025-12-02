73d2fe15ce73cb5dba0821a9bebbc40b694af288
Kori-njoroge <korinjoroge63@gmail.com>
2025-12-02 14:42:32 +0300

    returning functions

--- Files/functions.py
+++ Files/functions.py
@@ -9,3 +9,10 @@ def greeting1( name ):
     print (f"Hello World, {name}")
     
 greeting1()
+
+# return items from a function
+def sum_of(a, b):
+    sum = (a+b)
+    return sum
+    
+c = sum_of(3,4)
