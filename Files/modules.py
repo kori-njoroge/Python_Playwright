92899feb9995fef644aee716a3f368a00ba11a53
Kori-njoroge <korinjoroge63@gmail.com>
2025-12-02 15:25:01 +0300

    Modules

--- Files/modules.py
+++ Files/modules.py
@@ -1,6 +1,6 @@
 
-import functions as f
-from functions import sum_of as s
+import Python_Playwright.Files.my_modules.functions as f
+from Python_Playwright.Files.my_modules.functions import sum_of as s
 
 f.sum_of_product(1,2)
 s(2,3)
