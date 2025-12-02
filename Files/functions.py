abe8ea84de12a1fe567eebc49eecf4884635f4c0
Kori-njoroge <korinjoroge63@gmail.com>
2025-12-02 14:48:48 +0300

    functions annotations

--- Files/functions.py
+++ Files/functions.py
@@ -16,3 +16,12 @@ def sum_of(a, b):
     return sum
     
 c = sum_of(3,4)
+
+# function annotations
+a = 'hi'
+a = 45
+
+def sum_of_product(a: int, b:int) -> int:
+    return a + b
+c = sum_of_product(3,4)
+c =sum_of_product('Hello', "there")
